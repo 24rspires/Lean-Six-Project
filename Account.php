4ec217705f4359606fc71073dbc8819d21b421f3
Atkins <24AtkinsL@mydacc.org>
2024-04-01 11:45:50 -0400

    account

---
+++ Account.php
@@ -0,0 +1,19 @@
+
+
+<?PHP
+
+class Account {
+    public username;
+    public password;
+    public __construct()
+    {
+        // riley code go here
+    }
+    public tryLogin($username, $password)
+    {
+        // more riley code
+    }
+    
+}
+
+?>
