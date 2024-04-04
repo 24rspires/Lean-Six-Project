c94e31b6e994c11f3350a1dbecf0e9bb85a4f07a
Thomas <24ThomasL@mydacc.org>
2024-04-04 12:32:05 -0400

    nav bar

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css">
         <title>Boker Real Estate | Home</title>
     </head>
     <body>
         <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>ewfg
+            <ul class="Nav-Bar">
+                <li><a href="index.php">Home</a></li>
                 <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
+            
         <?PHP
         include_once "General.php";
         include_once "Account.php";
