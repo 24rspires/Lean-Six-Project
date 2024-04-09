c1fdd10f809940a70fc4ec996ed442aa442e0a62
Thomas <24ThomasL@mydacc.org>
2024-04-09 13:53:13 -0400

    for skye

--- index.php
+++ index.php
@@ -53,17 +53,18 @@
 
         
         
-            <div class="container">
-                <div class="bg-box">
-                    <img src="" alt="no images">
-                </div>
+        <div class="container">
+            <div class="bg-box">
+                <img src="the honored one.png" alt="no images">
             </div>
-            <section class="search">
+            <section>
                 <form method="POST">
-                    <input type="text" name="field" placeholder="Search for Zipcode, Address or State">
-                    <br>
-                    <input type="submit" name="search">
+                    <div class="search-container">
+                        <input type="text" name="search" placeholder="Search for Zipcode, Address or State">
+                        <input type="submit" value="Search">
+                    </div>
                 </form>
             </section>
+        </div>
     </body>
 </html>
