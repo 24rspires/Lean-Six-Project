8211c8105a2ecb7408e0abd413a466a75843f048
Thomas <24ThomasL@mydacc.org>
2024-04-10 11:43:48 -0400

    agents

--- UIHelper.php
+++ UIHelper.php
@@ -26,6 +26,7 @@ class UIHelper
                     <ul class='Nav-Bar'>
                         <li><a href='index.php'>Home</a></li>
                         <li><a href='login.php'>Login</a></li>
+                        <li><a href='agents.php'>Agents</a></li>
                     </ul>
                 </nav>";
         } else {
