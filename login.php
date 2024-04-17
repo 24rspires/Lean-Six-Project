83eb17741eeea7405c3cf068ed40fbd0d35fcb86
Thomas <24ThomasL@mydacc.org>
2024-04-17 13:06:48 -0400

    nav-bar fixes

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register | Boker Real Estate</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/login.css">
     <link rel="stylesheet" href="./css/nav-bar.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
