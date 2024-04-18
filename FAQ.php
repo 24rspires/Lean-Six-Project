b61f6128f8d904bfa6da56d6918aa181e7274b8f
Thomas <24ThomasL@mydacc.org>
2024-04-18 12:21:03 -0400

    FAQ

---
+++ FAQ.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+    <body>
+        <div class="d-flex container justify-content-center">
+            <div class="row">
+                <h1>test</h1>
+            </div>
+        </div>
+
+        <section class=" d-flex container justify-content-center align-self-center mt-5 bg-box">
+            <div class="row">
+                <div class="col-md-6 ">
+                    <ul>
+                        <li>
+                            test
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
