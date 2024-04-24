c2ed1de216791eb31fca7f4e1051b3d00938936d
Spires <24SpiresR@mydacc.org>
2024-04-24 11:58:55 -0400

    Add navbar and styling to property.php

--- property.php
+++ property.php
@@ -9,44 +9,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/property.css">
+    <link rel="stylesheet" href="./css/nav-bar.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/d316673763.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-<style>
-    .image-container {
-        height: 635px;
-        width: 100%;
-        overflow: hidden;
-        margin-bottom: 5px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    },
-    .image-container-image {
-        height: 100%;
-        width: 100%;
-        object-fit: none;
-        object-position: center;
-    }
-    .big-image {
-        box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.3);
-        border-radius: 4px;
-    }
-    .big-image:hover {
-        cursor: pointer
-    }
-</style>
 
 <?PHP
 include_once "Properties.php";
 include_once "UIHelper.php";
+
+UIHelper::navBar();
 
 class ImageGetter
 {
