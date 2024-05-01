f26ce35fc779ea957cea805f650e12f8ee35b0e1
Spires <24SpiresR@mydacc.org>
2024-05-01 11:52:41 -0400

    LUKE

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title id="dynamicTitle">boker search</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -91,7 +91,7 @@
                         </div>
                         <div class="row">
                             <h2>Bedrooms</h2>
-                            <div class="col m-0 p-0 justify-content-center">
+                            <div class="row m-0 p-0 justify-content-center">
                                 <button class="radio" checked name="bedroom" value="0" type="button">
                                     Any
                                 </button>
@@ -119,7 +119,7 @@
                         Location
                     </button>
                     <div class="dropdown-menu">
-                        <div class="col">
+                        <div class="col slider-dropdown">
                             <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
                             <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
                             <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
@@ -205,6 +205,20 @@
 <!--    a;skldfjkl;ad-->
 
     <style>
+        #slider-range {
+            width: 80%;
+        }
+
+        .roomFrame {
+            height: 15em;
+            width: 30em;
+        }
+
+        .slider-dropdown {
+            height: 5em;
+            width: 20em;
+        }
+
         .modal-dialog,
         .modal-content {
             /* 80% of window height */
